9bfbb091e4beda72585d15600014a247cd8d534e
Jon Townsend <jon.townsend@limelyte.com>
2012-06-28 19:57:57 -0700

    Moved Caching bundle addition outside of dev bundles. Added transition to image when hovered.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,8 +22,9 @@ class AppKernel extends Kernel
             //new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
+        $bundles[] = new Caching\BlogBundle\CachingBlogBundle();
+
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Caching\BlogBundle\CachingBlogBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
